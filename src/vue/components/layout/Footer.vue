8fb15c336f7d1adb92e4d42456af9859b5a34adc
CrisFeit <cristiano.feitosa@corebiz.com.br>
2020-03-02 19:19:44 -0300

    fixed: imgs paths

--- src/vue/components/layout/Footer.vue
+++ src/vue/components/layout/Footer.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   footer.footer
     a(href="http://www.grupoboticario.com.br/pt/Paginas/Inicial.aspx" class="company" tabindex="0" target="_blank") &copy;
-      img(class="company-logo" src="@/assets/img/logo-group.png")
+      img(class="company-logo" src="img/logo-group.png")
     section.social
       p.social-title Siga a Eudora:
       include ../../includes/social-media.pug
